2ad851896abc04465b91d5bb421ba4e699b554f9
O020617 <O020617@O020617-PC.jiec.co.jp>
2017-04-19 13:47:50 +0900

    変更

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 //------------------------------------------------------------------------------
 
 //This application uses express as its web server
-//for more info, see: http://expressjs.com
+// for more info, see: http://expressjs.com
 var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
@@ -30,19 +30,77 @@ app.use(bodyParser.json());
 // Date()で現在時刻を取得するためのユーティリティ
 var dateutil = require('date-utils');
 
-app.post('/', function(req, res){
-var date = new Date();
-var now = date.toFormat("YYYY/MM/DD HH24:MI:SS");
-req.body.date = now;
+// Cloudant用アクセス・モジュール「cradle」設定
+var cradle = require('cradle');
 
-console.log('app.js req.body: ' + JSON.stringify(req.body));
-res.send(req.body);
+// Cloudant DB接続情報取得
+var services = JSON.parse(process.env.VCAP_SERVICES);
+var credentials = services['cloudantNoSQLDB'][0].credentials;
+var host = credentials.host;
+var port = credentials.port;
+var options = {
+ cache : true,
+ raw : false,
+ secure : true,
+ auth : {
+ username : credentials.username,
+ password : credentials.password
+ }
+};
+
+// データベース接続
+var db = new (cradle.Connection)(host, port, options).database('cldb');
+
+app.post('/', function(req, res){
+ var date = new Date();
+ var now = date.toFormat("YYYY/MM/DD HH24:MI:SS");
+ req.body.date = now;
+
+ // 項目の保存
+ db.save(now, req.body);
+  res.send(req.body);
 });
+
+// 「全件削除」ボタンの id=removeAll, ui_item.jsの url:'/removeAll'でcall
+app.post('/removeAll', function(req, res){
+
+ // 全件検索を、作成したview名 items_view にて実行
+ db.view('items/items_view', function (err, rows) {
+ if (!err) {
+ rows.forEach(function (id, row) {
+ db.remove(id);
+ console.log("removed key is: %s", id);
+ });
+ } else { console.log("app.js db.remove error: " + err); }
+
+ });
+
+ res.send({});
+});
+
+
+// 「全件表示」ボタンの id=getAll, ui_item.jsの url:'/getAll'でcall
+app.post('/getAll', function(req, res){
+ returnTable(res);
+});
+
+var returnTable = function(res) {
+ // 全件検索を、作成したview名 items_view にて実行
+ db.view('items/items_view', function (err, rows) {
+ if (!err) {
+ rows.forEach(function (id, row) {
+ console.log("key: %s, row: %s", id, JSON.stringify(row));
+ });
+ } else { console.log("app.js returnTable error: " + err); }
+
+ res.send(rows);
+ });
+}
 /** *ここまで追加** */
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 
-// print a message when the server starts listening
-console.log("server starting on " + appEnv.url);
+ // print a message when the server starts listening
+ console.log("server starting on " + appEnv.url);
 });
--- public/js/ui_table.js
+++ public/js/ui_table.js
@@ -1,30 +1,7 @@
 // ui_table.js ブラウザUI用 JavaScript (index.htmlより呼ばれる)
 
 $(function() {
-	// 追加ボタン（index.htmlのid=add）押下時 実行
-	$("#add").click(function(e) {
-		e.preventDefault(); // エラー
-		var param = {};
-		param.item1 = $("#item1").val() || ""; // 入力された文字の取得
-
-		// POSTでのajaxコールで、サーバーのapp.jsのapp.post呼び出し
-		$.ajax({
-			type : 'POST',
-			data : JSON.stringify(param),
-			contentType : 'application/json',
-			url : '/',
-			success : function(data) {
-				console.log('add success: ' + JSON.stringify(data));
-				showTable(data);
-			},
-			error : function(data) {
-				console.log('error ' + JSON.stringify(data));
-			}
-		});
-
-		// 入力項目名を空白に
-		$("#item1").val('');
-	}); // #add
+	console.log('ui_item.js in');
 
 	// サーバから取得したデータを、htmlテーブルに追加
 	var showTable = function(data) {
@@ -32,4 +9,74 @@ $(function() {
 				"<td>" + data.date + "</td>").append(
 				"<td>" + data.item1 + "</td>")
 	};
+
+	// 追加ボタン（index.htmlのid=add）押下時 実行
+	$("#add").click(function(e) {
+		e.preventDefault();
+		var param = {};
+		param.item1 = $("#item1").val() || "";
+
+		// POSTでのajaxコールで、サーバーのapp.jsのapp.post /add呼び出し
+		$.ajax({
+			type : 'POST',
+			data : JSON.stringify(param),
+			contentType : 'application/json',
+			url : '/',
+			success : function(data) {
+				console.log('success add: ' + JSON.stringify(data));
+				showTable(data);
+			},
+			error : function(data) {
+				console.log('error add: ' + JSON.stringify(data));
+			}
+		});
+
+		// 入力項目名を空白に
+		$("#item1").val('');
+	});
+
+	// 全件表示ボタン（index.htmlのid=getAll）押下時 実行
+	$("#getAll").click(function(e) {
+		e.preventDefault();
+		$("#tableItems").empty();
+
+		// POSTでのajaxコールで、サーバーのapp.jsのapp.post /getAll呼び出し
+		$.ajax({
+			type : 'POST',
+			data : {},
+			contentType : 'application/json',
+			url : '/getAll',
+			success : function(rows) {
+				for (var i = 0; i < rows.length; i++) {
+					console.log(' row ' + i + ": " + JSON.stringify(rows[i]));
+					showTable(rows[i].value);
+
+				}
+			},
+			error : function(data) {
+				console.log('error getAll: ' + JSON.stringify(data));
+			}
+		});
+	});
+
+	// 全件削除ボタン（index.htmlのid=removeAll）押下時 実行
+	$("#removeAll").click(function(e) {
+		e.preventDefault();
+
+		// POSTでのajaxコールで、サーバーのapp.jsのapp.post呼び出し
+		$.ajax({
+			type : 'POST',
+			data : {},
+			contentType : 'application/json',
+			url : '/removeAll',
+			success : function(data) {
+				console.log('success removeAll');
+			},
+			error : function(data) {
+				console.log('error getAll: ' + JSON.stringify(data));
+			}
+		});
+
+		$("#tableItems").empty();
+	});
 });
