05392fcc486f00bce7208a486f07671266aab40a
O020617 <O020617@O020617-PC.jiec.co.jp>
2017-04-19 13:20:38 +0900

    変更

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
+//This application uses express as its web server
+//for more info, see: http://expressjs.com
 var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
@@ -21,8 +21,28 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+/** *ここから追加** */
+// POSTパラメータ取得用 body-parser設定 (express4から必要)
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Date()で現在時刻を取得するためのユーティリティ
+var dateutil = require('date-utils');
+
+app.post('/', function(req, res){
+var date = new Date();
+var now = date.toFormat("YYYY/MM/DD HH24:MI:SS");
+req.body.date = now;
+
+console.log('app.js req.body: ' + JSON.stringify(req.body));
+res.send(req.body);
+});
+/** *ここまで追加** */
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+
+// print a message when the server starts listening
+console.log("server starting on " + appEnv.url);
 });
